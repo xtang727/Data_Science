0f4db772402cc309723a9cab8e3b47310c8cf11d
xtang727 <xtang727@outlook.com>
2022-01-01 20:30:01 -0500

    Revised to update function using latest releases

--- 19. Predicting the stock market/predict.py
+++ 19. Predicting the stock market/predict.py
@@ -4,18 +4,19 @@ from datetime import datetime
 
 df = pd.read_csv('sphist.csv')
 df['DateTime'] = pd.to_datetime(df.Date)
-df_ordered = df.sort('DateTime', ascending=True)
+df_ordered = df.sort_values('DateTime', ascending=True)
 df_ordered['index'] = range(0,df.shape[0],1)
-df_ordered.set_index(['index'])
+df_ordered = df_ordered.set_index(['index'])
 
 
 df_ordered['date_after_april1_2015'] = df_ordered.DateTime > datetime(year=2015, month=4, day=1)
-data_mean_5day = pd.rolling_mean(df_ordered.Close, window=5).shift(1)
-data_mean_365day = pd.rolling_mean(df_ordered.Close, window=365).shift(1)
+
+data_mean_5day = df_ordered.Close.rolling(5).mean().shift(1)
+data_mean_365day = df_ordered.Close.rolling(365).mean().shift(1)
 data_mean_ratio = data_mean_5day/data_mean_365day
 
-data_std_5day = pd.rolling_std(df_ordered.Close, window=5).shift(1)
-data_std_365day = pd.rolling_std(df_ordered.Close, window=365).shift(1)
+data_std_5day = df_ordered.Close.rolling(5).std().shift(1)
+data_std_365day = df_ordered.Close.rolling(365).std().shift(1)
 data_std_ratio = data_std_5day/data_std_365day
 
 df_ordered['data_mean_5day'] = data_mean_5day
